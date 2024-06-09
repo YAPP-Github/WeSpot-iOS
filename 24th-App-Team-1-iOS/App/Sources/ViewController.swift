4c9b54613cef8aadcbd9a0e318b76580363f2f3e
Do-hyun-Kim <dohyun8032@gmail.com>
2024-06-09 22:04:19 +0900

    feat: Tuist UIKit 전용 폴더 및 파일 생성

---
+++ 24th-App-Team-1-iOS/App/Sources/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Kim dohyun on 6/9/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ 24th-App-Team-1-iOS/Project.swift
@@ -0,0 +1,31 @@
+import ProjectDescription
+
+let project = Project(
+    name: "App",
+    targets: [
+        .target(
+            name: "App",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.App",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                ]
+            ),
+            sources: ["App/Sources/**"],
+            resources: ["App/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "AppTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.AppTests",
+            infoPlist: .default,
+            sources: ["App/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "App")]
+        ),
+    ]
+)
