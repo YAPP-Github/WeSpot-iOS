9096b9367f7457f9deec4947d595ffaeb366aa81
SeoU <tjdnfkd1225@naver.com>
2024-06-13 21:53:36 +0900

    feat: Makefile을 통해 Xcode File Templates 설치 기능 추가
    - 'templates' 타겟을 Makefile에 추가하여 Xcode File Templates 설치
    - 템플릿 파일을 ~/Library/Developer/Xcode/Templates/feature.xctemplate 경로에 설치
    - clean이 tuist에 영향을 주지 않도록 템플릿 파일만 삭제하도록 설정

---
+++ 24th-App-Team-1-iOS/Templates/feature.xctemplate/___FILEBASENAME___Feature.swift
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
