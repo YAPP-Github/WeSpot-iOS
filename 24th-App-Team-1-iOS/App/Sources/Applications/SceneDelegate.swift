ac21cc1b064ae2b3492edf5c423a4c44cd76044a
Do-hyun-Kim <dohyun8032@gmail.com>
2024-06-27 17:11:43 +0900

    fix: feature, domain, service, core default infoPlist 추가 - App Target Custom InfoPlist 추가 - Settings+Templates 파일 생성 - Settings 내부 makeSettings 메서드에 RunPath_search_Paths에 flag 값, Library_Search_Paths flag 값 추가 - Applications 폴더 생성 및 AppDelegate, SceneDelegate 파일 추가 - LaunchScreen, Assets 파일 추가

---
+++ 24th-App-Team-1-iOS/App/Sources/Applications/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  wespot
+//
+//  Created by Kim dohyun on 6/27/24.
+//
+
+import UIKit
+import ReactorKit
+
+
+class SceneDelegate: UIResponder, UISceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        //TODO: ViewControle 생성 해서 RootViewController로 지정 해주세요
+        window?.makeKeyAndVisible()
+        
+    }
+    
+}
