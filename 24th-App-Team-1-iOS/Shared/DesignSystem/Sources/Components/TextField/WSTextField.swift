7d950ad58dc85d949a81613290516e4fbe54f7cd
SeoU <tjdnfkd1225@naver.com>
2024-07-04 23:58:58 +0900

    feat: WSTextField 좌우 이미지 속성 및 레이아웃 추가, 텍스트필드 관련 이미지 추가

--- 24th-App-Team-1-iOS/Shared/DesignSystem/Sources/Components/TextField/WSTextField.swift
+++ 24th-App-Team-1-iOS/Shared/DesignSystem/Sources/Components/TextField/WSTextField.swift
@@ -7,20 +7,51 @@
 
 import UIKit
 
+import SnapKit
+import Then
 import RxSwift
 import RxCocoa
 
-final public class WSTextField: UITextField {
+public final class WSTextField: UITextField {
+    
+    public enum TextFieldState {
+        case `default`
+        case withLeftItem(UIImage)
+        case withRightItem(UIImage)
+        
+        var padding: UIEdgeInsets {
+            switch self {
+            case .default:
+                    .init(top: 18, left: 18, bottom: 18, right: 18)
+            case .withLeftItem:
+                    .init(top: 18, left: 59, bottom: 18, right: 18)
+            case .withRightItem:
+                    .init(top: 18, left: 20, bottom: 18, right: 48)
+            }
+        }
+    }
     
     // MARK: - Properties
     private let disposeBag = DisposeBag()
-    private let defaultPadding = UIEdgeInsets(top: 18, left: 18, bottom: 18, right: 18)
     private var placeholderText: String = "Placeholder"
+    private var titleText: String?
+    private var textFieldState: TextFieldState = .default
     
-    // MARK: - init
-    public init(placeholder: String = "Placeholder") {
+    // MARK: - Components
+    private let titleLabel =  WSLabel(wsFont: .Body04, textAlignment: .left).then {
+        $0.textColor = DesignSystemAsset.Colors.gray100.color
+    }
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    
+    // MARK: - Initializer
+    public init(state: TextFieldState = .default, placeholder: String = "Placeholder", title: String? = nil) {
         super.init(frame: .zero)
         
+        self.textFieldState = state
+        self.titleText = title
         self.placeholderText = placeholder
         setupUI()
         bindUI()
@@ -32,14 +63,26 @@ final public class WSTextField: UITextField {
     
     // MARK: - Functions
     private func setupUI() {
+        
+        switch textFieldState {
+        case .default:
+            break
+        case .withLeftItem(let image):
+            setupImageView(image, isLeft: true)
+        case .withRightItem(let image):
+            setupImageView(image, isLeft: false)
+        }
+        
+        if let titleText = titleText {
+            setupTitleLabel(with: titleText)
+        }
         layer.borderColor = UIColor.clear.cgColor
         layer.borderWidth = 1.0
         layer.cornerRadius = 12
         placeholder = placeholderText
         attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [NSAttributedString.Key.foregroundColor: DesignSystemAsset.Colors.gray400.color])
         backgroundColor = DesignSystemAsset.Colors.gray700.color
-       applyWSFont(.Body04)
-        
+        applyWSFont(.Body04)
     }
     
     private func bindUI() {
@@ -73,7 +116,7 @@ final public class WSTextField: UITextField {
     }
     
     // WSFont를 적용하는 메소드 추가
-    public func applyWSFont(_ wsFont: WSFont) {
+    private func applyWSFont(_ wsFont: WSFont) {
         self.font = wsFont.font()
         
         if let text = self.text, !text.isEmpty {
@@ -98,18 +141,47 @@ final public class WSTextField: UITextField {
         ]
     }
     
+    // title Label 레이아웃 설정
+    private func setupTitleLabel(with titleText: String) {
+        addSubview(titleLabel)
+        titleLabel.text = titleText
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(self.snp.top).offset(-12)
+        }
+    }
+    
+    // UIImageView 레이아웃 설정
+    private func setupImageView(_ image: UIImage, isLeft: Bool) {
+        imageView.image = image
+        addSubview(imageView)
+        
+        imageView.snp.makeConstraints {
+            if isLeft {
+                $0.leading.equalToSuperview().offset(16)
+            } else {
+                $0.trailing.equalToSuperview().offset(-8)
+            }
+            $0.verticalEdges.equalToSuperview().inset(10)
+            $0.size.equalTo(40)
+        }
+    }
+
+    
     // 텍스트 필드의 텍스트 영역에 패딩을 적용
     public override func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by:defaultPadding)
+        return bounds.inset(by: textFieldState.padding)
     }
     
     public override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: defaultPadding)
+        return bounds.inset(by: textFieldState.padding)
     }
     
     public override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: defaultPadding)
+        return bounds.inset(by: textFieldState.padding)
     }
-
+    
     
 }
