6915b1e8d080bcfca3b051e6c30df141cb0092b1
SeoU <tjdnfkd1225@naver.com>
2024-07-15 05:24:41 +0900

    feat: 회원가입 이름 등록 UI 구현
    - 이름 선택 뷰 SignUpNameViewController & SignUpNameViewReactor 구현

---
+++ 24th-App-Team-1-iOS/Feature/LoginFeature/Sources/Presentation/Reactors/SignUpNameViewReactor.swift
@@ -0,0 +1,84 @@
+//
+//  SignUpNameViewReactor.swift
+//  LoginFeature
+//
+//  Created by eunseou on 7/12/24.
+//
+
+import Foundation
+
+import ReactorKit
+
+public final class SignUpNameViewReactor: Reactor {
+    
+    public struct State {
+        var name: String = ""
+        var errorMessage: String?
+        var isButtonEnabled: Bool = false
+        var isWarningHidden: Bool = true
+    }
+    
+    public enum Action {
+        case inputName(String)
+        case nextButtonTap
+    }
+    
+    public enum Mutation {
+        case setName(String)
+        case setErrorMessage(String?)
+        case setButtonEnabled(Bool)
+        case setWarningHidden(Bool)
+    }
+    
+    public var initialState: State
+    
+    public init() {
+        self.initialState = State()
+    }
+    
+    public func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .inputName(let name):
+            let isValid = self.validateName(name)
+            let errorMessage = isValid ? nil : (name.count <= 1 ? nil : "2~5자의 한글만 입력 가능해요")
+            let isButtonEnabled = name.count >= 2 && isValid
+            let isWarningHidden = name.count <= 1
+            
+            return Observable.concat([
+                Observable.just(Mutation.setName(name)),
+                Observable.just(Mutation.setErrorMessage(errorMessage)),
+                Observable.just(Mutation.setButtonEnabled(isButtonEnabled)),
+                Observable.just(Mutation.setWarningHidden(isWarningHidden))
+            ])
+            
+        case .nextButtonTap:
+            return .just(.setButtonEnabled(true))
+        }
+    }
+    
+    public func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+        case .setName(let name):
+            newState.name = name
+            
+        case .setErrorMessage(let errorMessage):
+            newState.errorMessage = errorMessage
+            
+        case .setButtonEnabled(let isEnabled):
+            newState.isButtonEnabled = isEnabled
+            
+        case .setWarningHidden(let isHidden):
+            newState.isWarningHidden = isHidden
+        }
+        
+        return newState
+    }
+    
+    private func validateName(_ name: String) -> Bool {
+        let regex = "^[가-힣]{2,5}$"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        return predicate.evaluate(with: name)
+    }
+}
