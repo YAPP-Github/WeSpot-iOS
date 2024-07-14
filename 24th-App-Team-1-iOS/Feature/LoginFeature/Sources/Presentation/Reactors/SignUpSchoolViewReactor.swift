d477973185fbecdda7e90a2838989e50ca367d1f
SeoU <tjdnfkd1225@naver.com>
2024-07-15 04:52:15 +0900

    feat: 회원가입 학교 등록 UI 구현
    - 일회용 컴포넌트 GradientView 구현 및 학교검색 결과 셀 SchoolSearchTableViewCell 구현
    - 학교 검색 뷰 SignUpSchoolViewController & SignUpSchoolViewReactor 구성

---
+++ 24th-App-Team-1-iOS/Feature/LoginFeature/Sources/Presentation/Reactors/SignUpSchoolViewReactor.swift
@@ -0,0 +1,62 @@
+//
+//  SignUpSchoolViewReactor.swift
+//  LoginFeature
+//
+//  Created by eunseou on 7/12/24.
+//
+
+import Foundation
+
+import ReactorKit
+
+public final class SignUpSchoolViewReactor: Reactor {
+    
+    
+    public struct State {
+        var schoolName: String = ""
+        var schoolList: [String] = []
+        var selectedSchool: String?
+    }
+    
+    public enum Action {
+        case searchSchool(String)
+        case selectSchool(String)
+    }
+    
+    public enum Mutation {
+        case setSchoolList(String, [String])
+        case setSelectedSchool(String?)
+    }
+    
+    public var initialState: State = State()
+    
+    public init() {
+        self.initialState = State()
+    }
+    
+    public func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .searchSchool(let schoolName):
+            if schoolName.isEmpty {
+                return .just(.setSchoolList(schoolName, []))
+            }
+            let randomCount = Int.random(in: 0...3)
+            let schoolList = Array(repeating: "test", count: randomCount)
+            return .just(.setSchoolList(schoolName, schoolList))
+        case .selectSchool(let schoolName):
+            return .just(.setSelectedSchool(schoolName))
+        }
+    }
+    
+    public func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setSchoolList(let schoolName, let results):
+            newState.schoolName = schoolName
+            newState.schoolList = results
+        case .setSelectedSchool(let schoolName):
+            newState.selectedSchool = schoolName
+        }
+        return newState
+    }
+}
---
+++ 24th-App-Team-1-iOS/Feature/LoginFeature/Sources/Presentation/ViewControllers/SignUpSchoolViewController.swift
@@ -0,0 +1,167 @@
+//
+//  SignUpSchoolViewController.swift
+//  LoginFeature
+//
+//  Created by eunseou on 7/12/24.
+//
+
+import UIKit
+import Util
+import DesignSystem
+
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+import ReactorKit
+
+public final class SignUpSchoolViewController: BaseViewController<SignUpSchoolViewReactor> {
+
+    //MARK: - Properties
+    private let titleLabel = WSLabel(wsFont: .Header01, text: "학교 검색")
+    private let subTitleLabel = WSLabel(wsFont: .Body06, text: "현재 재학 중인 학교 기준으로 검색해 주세요")
+    private let schoolTextField = WSTextField(state: .withLeftItem(DesignSystemAsset.Images.magnifyingglass.image), placeholder: "학교 이름으로 검색해 주세요")
+    private let warningLabel = WSLabel(wsFont: .Body07, text: "20자 이내로 검색해 주세요")
+    private let additionalButton = UIButton()
+    private let additionalButtonLine = UIView()
+    private let schoolSearchTableView = UITableView().then {
+        $0.register(SchoolSearchTableViewCell.self, forCellReuseIdentifier: "SchoolSearchTableViewCell")
+        $0.backgroundColor = .clear
+        $0.separatorStyle = .none
+        $0.rowHeight = 104
+    }
+    private let gradientView = GradientView()
+    private let nextButton = WSButton(wsButtonType: .default(12)).then {
+        $0.setupButton(text: "다음")
+    }
+    
+    //MARK: - LifeCycle
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        schoolTextField.becomeFirstResponder()
+        warningLabel.isHidden = true
+    }
+    
+    //MARK: - Configure
+    public override func setupUI() {
+        super.setupUI()
+        
+        view.addSubviews(titleLabel, subTitleLabel, schoolTextField, warningLabel, additionalButton,
+                         additionalButtonLine, schoolSearchTableView, gradientView, nextButton)
+    }
+    
+    public override func setupAutoLayout() {
+        super.setupAutoLayout()
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(30)
+        }
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(9)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(30)
+        }
+        schoolTextField.snp.makeConstraints {
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(16)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(60)
+        }
+        warningLabel.snp.makeConstraints {
+            $0.top.equalTo(schoolTextField.snp.bottom).offset(4)
+            $0.leading.equalTo(schoolTextField.snp.leading).offset(10)
+        }
+        additionalButton.snp.makeConstraints {
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(21)
+            $0.top.equalTo(schoolTextField.snp.bottom).offset(24)
+        }
+        additionalButtonLine.snp.makeConstraints {
+            $0.top.equalTo(additionalButton.snp.bottom)
+            $0.height.equalTo(1)
+            $0.horizontalEdges.equalTo(additionalButton.snp.horizontalEdges)
+        }
+        schoolSearchTableView.snp.makeConstraints {
+            $0.top.equalTo(schoolTextField.snp.bottom).offset(24)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-44)
+        }
+        nextButton.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(52)
+            $0.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-12)
+        }
+        gradientView.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(nextButton.snp.bottom).offset(12)
+            $0.height.equalTo(110)
+        }
+    }
+    
+    public override func setupAttributes() {
+        super.setupAttributes()
+        
+        view.backgroundColor = DesignSystemAsset.Colors.gray900.color
+    
+        navigationBar
+            .setNavigationBarUI(property: .leftWithCenterItem(DesignSystemAsset.Images.arrow.image, "회원가입"))
+            .setNavigationBarAutoLayout(property: .leftWithCenterItem)
+        
+        titleLabel.textColor = DesignSystemAsset.Colors.gray100.color
+        
+        subTitleLabel.textColor = DesignSystemAsset.Colors.gray400.color
+        
+        warningLabel.textColor = DesignSystemAsset.Colors.destructive.color
+        
+        additionalButton.setTitle("찾는 학교가 없다면?", for: .normal)
+        additionalButton.setTitleColor(DesignSystemAsset.Colors.gray300.color, for: .normal)
+        additionalButton.titleLabel?.font = WSFont.Body05.font()
+        
+        additionalButtonLine.backgroundColor = DesignSystemAsset.Colors.gray300.color
+    }
+    
+    public override func bind(reactor: Reactor) {
+        super.bind(reactor: reactor)
+        
+        schoolTextField.rx.controlEvent([.editingDidBegin, .editingDidEnd])
+            .map { self.schoolTextField.isEditing }
+            .bind(to: schoolTextField.borderUpdateBinder )
+            .disposed(by: disposeBag)
+        
+        schoolTextField.rx.text.orEmpty
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .map { Reactor.Action.searchSchool($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.schoolList }
+            .bind(to: schoolSearchTableView.rx.items(cellIdentifier: "SchoolSearchTableViewCell", cellType: SchoolSearchTableViewCell.self)) { (index, school, cell) in
+                
+                cell.selectionStyle = .none
+                cell.setupCell(schoolName: "학교이름\(index+1)", address: "주소\(index+1)")
+            }
+            .disposed(by: disposeBag)
+            
+        reactor.state
+            .map { $0.schoolName.isEmpty || (!$0.schoolName.isEmpty && $0.schoolList.count > 0)}
+            .bind(to: additionalButton.rx.isHidden, additionalButtonLine.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.schoolName.count <= 20 }
+            .bind(to: warningLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                let signUpGradeViewReactor = SignUpGradeViewReactor()
+                let signUpGradeViewController = SignUpGradeViewController(reactor: signUpGradeViewReactor)
+                owner.navigationController?.pushViewController(signUpGradeViewController, animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
+}
