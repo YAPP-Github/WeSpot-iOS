921e18995ce1b906f9bf96d46a21a64bb8e30b9d
SeoU <tjdnfkd1225@naver.com>
2024-07-29 16:04:56 +0900

    fix: tuist 의존성 수정

--- 24th-App-Team-1-iOS/Core/Extensions/Sources/Reactive+Extensions.swift
+++ 24th-App-Team-1-iOS/Core/Extensions/Sources/Reactive+Extensions.swift
@@ -6,21 +6,9 @@
 //
 
 import UIKit
-import Networking
 
 import RxSwift
 import RxCocoa
-
-extension ObservableType where Element == Data {
-    public func decodeMap<T: Decodable>(_ type: T.Type) -> Observable<T> {
-        return map { data in
-            guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
-                throw WSNetworkError.default(message: "Decoding Error")
-            }
-            return decodedData
-        }
-    }
-}
 
 extension Reactive where Base: UIViewController {
     public var viewWillAppear: ControlEvent<Bool> {
