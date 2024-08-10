b45901bbee94063a9bb2b56a6a9ceba8fe316f55
SeoU <tjdnfkd1225@naver.com>
2024-08-11 01:35:53 +0900

    feat: component Reator Extensions 파일 분리

--- 24th-App-Team-1-iOS/Feature/MessageFeature/Sources/Presentation/ViewControllers/MessageHomeViewController.swift
+++ 24th-App-Team-1-iOS/Feature/MessageFeature/Sources/Presentation/ViewControllers/MessageHomeViewController.swift
@@ -175,23 +175,3 @@ public final class MessageHomeViewController: BaseViewController<MessageHomeView
     }
 }
 
-private extension Reactive where Base: WSBanner {
-    var reservedMessages: Binder<Int> {
-        return Binder(self.base) { view, count in
-            if count == 3 {
-                view.setTitleText("예약 중인 쪽지 \(count)개")
-            } else {
-                view.setTitleText("예약 중인 쪽지 \(count)개")
-                view.setSubTitleText("\(3 - count)개의 쪽지를 더 보낼 수 있어요")
-            }
-        }
-    }
-}
-
-private extension Reactive where Base: MessageCardView {
-    var isSendAllowed: Binder<Bool> {
-        return Binder(self.base) { view, isAllowed in
-            view.setSendAllowed(isAllowed)
-        }
-    }
-}
