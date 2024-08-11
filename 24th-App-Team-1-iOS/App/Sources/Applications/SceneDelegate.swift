3214ac7e2f077d24376e2dfd4fb509a62238fdfc
SeoU <tjdnfkd1225@naver.com>
2024-08-11 19:52:01 +0900

    feat: Message ReportView DI 주입 및 생성

--- 24th-App-Team-1-iOS/App/Sources/Applications/SceneDelegate.swift
+++ 24th-App-Team-1-iOS/App/Sources/Applications/SceneDelegate.swift
@@ -46,6 +46,7 @@ public class SceneDelegate: UIResponder, UISceneDelegate {
             MessageMainPresentationAssembly(),
             MessagePagePresentationAssembly(),
             MessageHomePresentationAssembly(),
+            MessageReportPresentationAssembly(),
             DataAssembly(),
             DomainAssembly()
         ])
