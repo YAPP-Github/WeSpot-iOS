5b4901e2c14722f91e727f1660ae4c5c6db03dfd
SeoU <tjdnfkd1225@naver.com>
2024-08-23 14:02:34 +0900

    feat: commonRepository 인스턴스 주입

--- 24th-App-Team-1-iOS/Core/Networking/Sources/Implement/implementation/ServiceImpl/WSNetworkService.swift
+++ 24th-App-Team-1-iOS/Core/Networking/Sources/Implement/implementation/ServiceImpl/WSNetworkService.swift
@@ -17,8 +17,9 @@ public final class WSNetworkService: WSNetworkServiceProtocol {
     private let session: Session = {
         let networkMonitor: WSNetworkMonitor = WSNetworkMonitor()
         let networkConfigure: URLSessionConfiguration = URLSessionConfiguration.af.default
-        let interceptor = (CommonRepositoryProtocol.self as? CommonRepositoryProtocol)
-                .map { WSNetworkInterceptor(commonRepository: $0) }
+        let commonRepository = CommonRepository()
+        let interceptor = WSNetworkInterceptor(commonRepository: commonRepository)
+               
         let networkSession: Session = Session(
             configuration: networkConfigure,
             interceptor: interceptor,
